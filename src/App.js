1b072d1e0704b8ba923cee713883bdc4b606f390
Gouravsaha12 <gouravsaha123098@gmail.com>
2024-12-31 23:53:22 +0530

    Initialize React app with Tailwind CSS, add essential files and configurations

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App bg-black h-screen">
+      <h1 className="text-9xl font-extrabold text-blue-600">HI</h1>
+    </div>
+  );
+}
+
+export default App;
